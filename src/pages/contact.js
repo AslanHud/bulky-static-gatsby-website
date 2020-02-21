dc8bbe260d1b67c38a5076496f11466a9d3bc56e
Aslambek Hudajev <aslambek.hudajev@bulkymedia.com>
2020-02-21 23:34:26 +0100

    About and Contact page added.

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact us</h1>
+            <h2>This is the contact page.</h2>
+        </div>
+    )
+}
+
+export default ContactPage;
