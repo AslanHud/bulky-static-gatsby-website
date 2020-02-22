6d960c7258bcbe40b372538d5e33aef699bb256a
Aslambek Hudajev <aslambek.hudajev@bulkymedia.com>
2020-02-22 12:54:46 +0100

    added links between pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
     return (
         <div>
             <h1>About</h1>
-            <h2>This is the about page.</h2>
+            <h2>We are bulky media and we are a modern web design beaureu based out of Uppsala, Sweden.</h2>
+            <p>Does your business need a website? <Link to="/contact">Contact us.</Link></p>
         </div>
     )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const ContactPage = () => {
     return (
         <div>
             <h1>Contact us</h1>
             <h2>This is the contact page.</h2>
+            <a href="https://www.twitter.com">Follow us on Twitter!</a>
         </div>
     )
 }
