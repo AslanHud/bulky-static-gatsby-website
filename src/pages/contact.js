a6cd3a4b430118377f198a4494303bdd40fd93bd
Aslambek Hudajev <aslambek.hudajev@bulkymedia.com>
2020-02-22 15:30:42 +0100

    Fixed conflicts, merging Win and Mac builds

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,21 +1,12 @@
 import React from 'react'
-<<<<<<< HEAD
 import Layout from '../components/layout'
-=======
-import { Link } from 'gatsby'
->>>>>>> 6d960c7258bcbe40b372538d5e33aef699bb256a
 
 const ContactPage = () => {
     return (
         <Layout>
             <h1>Contact us</h1>
             <h2>This is the contact page.</h2>
-<<<<<<< HEAD
         </Layout>
-=======
-            <a href="https://www.twitter.com">Follow us on Twitter!</a>
-        </div>
->>>>>>> 6d960c7258bcbe40b372538d5e33aef699bb256a
     )
 }
 
