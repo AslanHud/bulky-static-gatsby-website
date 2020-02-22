efbd3ba5c1113ce77c2725eccf35d7652b5fe562
Aslambek Hudajev <aslambek.hudajev@bulkymedia.com>
2020-02-22 15:38:11 +0100

    Merge complete

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
-<<<<<<< HEAD
 import Layout from '../components/layout'
-=======
->>>>>>> 6d960c7258bcbe40b372538d5e33aef699bb256a
 
 const AboutPage = () => {
     return (
         <Layout>
             <h1>About</h1>
-<<<<<<< HEAD
             <h2>This is the about page.</h2>
             <p>Need a developer? <Link to="/contact">Contact us!</Link></p>
         </Layout>
-=======
-            <h2>We are bulky media and we are a modern web design beaureu based out of Uppsala, Sweden.</h2>
-            <p>Does your business need a website? <Link to="/contact">Contact us.</Link></p>
-        </div>
->>>>>>> 6d960c7258bcbe40b372538d5e33aef699bb256a
     )
 }
 
