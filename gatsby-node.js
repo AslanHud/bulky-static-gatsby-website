8f180f7a0f9ed086c2d4671f90cd0ffc7de9580d
Aslambek Hudajev <aslambek.hudajev@bulkymedia.com>
2020-02-24 12:36:36 +0100

    Added a gatsby node API script that gets a slug from each markdown post.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require('path')
+
+//When a new markdown node is created, create a slug (equal to the markdown filename) that can then be used to
+//create unique URLs for each blog post. Then use that slug and make it into a field for each MarkdownRemark node.
+module.exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions
+
+    if(node.internal.type === 'MarkdownRemark') {
+        const slug = path.basename(node.fileAbsolutePath, '.md')
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug
+        })
+    }
+}
---
+++ src/templates/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            <div>
+                This is a blog post.
+            </div>
+        </Layout>
+    )
+}
+
+export default Blog
